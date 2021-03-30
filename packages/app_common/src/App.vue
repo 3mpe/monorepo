2450df2f0e786bc5f69d0cfe428a0159ccd0bc46
cantanert <cantanert@gmail.com>
2021-03-31 01:21:11 +0300

    anchor elements added for walk through mono-repo fragments

--- packages/app_common/src/App.vue
+++ packages/app_common/src/App.vue
@@ -5,9 +5,21 @@
         >About</router-link
       >
     </div>
+    <div>
+      <Header />
+    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+  import Header from './components/Header'
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
--- packages/app_common/src/components/Header.vue
+++ packages/app_common/src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1 style="background-color: darkorange; color: white">This components is exposed from common app</h1>
+  <div>
+    <h1 style="background-color: darkorange; color: white">This components is exposed from common app</h1>
+    <a style="color: white" href="http://localhost:3002">APP_2</a> <br>
+    <a style="color: white" href="http://localhost:3001">APP_1</a> <br>
+    <a style="color: white" href="http://localhost:3000">APP_COMMON</a> <br>
+  </div>
 </template>
